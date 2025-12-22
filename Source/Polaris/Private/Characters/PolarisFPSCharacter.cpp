33485211060fef4fd482f5d96b131b0d7033a5ce
Brandon Davis <wowzerss90@yahoo.com>
2025-12-21 21:43:46 -0500

    Setup Player and FPS Camera.

---
+++ Source/Polaris/Private/Characters/PolarisFPSCharacter.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Polaris/Public/Characters/PolarisFPSCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+APolarisFPSCharacter::APolarisFPSCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("CameraBoom");
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 0.0f;
+	CameraBoom->SetRelativeLocation(FVector(10.f, 0.f, 90.f));
+
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(CameraBoom);
+}
+
+// Called when the game starts or when spawned
+void APolarisFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Polaris/Public/Characters/PolarisFPSCharacter.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PolarisFPSCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class POLARIS_API APolarisFPSCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APolarisFPSCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UCameraComponent> Camera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<USpringArmComponent> CameraBoom;
+};
