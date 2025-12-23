d161ae331400c13df239d092a7eba9b6ea8d6bc2
MilkBagBoii <johnleonardo207@gmail.com>
2025-12-23 10:57:11 -0500

    Quick Fix

--- Source/Polaris/Public/FoodWater.h
+++ Source/Polaris/Public/FoodWater.h
@@ -34,10 +34,10 @@ public:
 	float Thirst = 0.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VALUE")
-	float HungerMult = 1.0f;
+	float HungerMult = 0.001f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VALUE")
-	float ThirstMult = 0.1f;
+	float ThirstMult = 0.001f;
 
 	UFUNCTION(BlueprintCallable)
 	void CalculateAppetite(float _DeltaTime);
